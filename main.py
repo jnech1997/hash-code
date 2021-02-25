be3ca2f54f16940de7412d84e35a07a529fec409
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2021-02-25 16:36:00 -0500

    remove print statements

--- main.py
+++ main.py
@@ -17,36 +17,21 @@ def createGraph(filename):
     numStreets = 0
     numCars = 0
     bonus = 0
-
     # Strips the newline character
     for line in Lines:
         count += 1
         if count == 1:
             totalDuration, numIntersections, numStreets, numCars, bonus = line.split()
-            # print("\n")
-            # print("-------------------------------------")
-            # print("duration is: ", duration)
-            # print("numIntersections is: ", numIntersections)
-            # print("numStreets is: ", numStreets)
-            # print("numCars is: ", numCars)
-            # print("bonus is: ", bonus)
-            # print("-------------------------------------")
         elif (count <= 1 + int(numStreets)):
             startIntersect, endIntersect, streetName, streetTime = line.split()
-            # print("streetName is: ", streetName)
-            # print("start intersection is: ", startIntersect)
-            # print("end intersection is: ", endIntersect)
-            # print("streetTime is: ", streetTime)
             DG.add_edge(startIntersect, endIntersect)
             DG[startIntersect][endIntersect]['weight'] = int(streetTime)
             DG[startIntersect][endIntersect]['name'] = streetName
-
             streetTimes[streetName] = streetTime
         else:
             carInfo = line.split()
             numStreetsCarWantsToTravel = carInfo[0]
             path = carInfo[1:]
-            #print("number of streets car wants to travel is: ", numStreetsCarWantsToTravel)
             for i in range(len(path)):
                 street = path[i]
                 if i > 0:
@@ -59,19 +44,14 @@ def createGraph(filename):
                         carStarts[street] += 1
                     else:
                         carStarts[street] = 1
-
     return DG
             
 def main(filename):
     DG = createGraph(filename)
-    # print("STREET HITS HASHMAP", streetHits)
     sortedStreetHits = sorted(streetHits.items(), key=lambda v: v[1], reverse=True)
     mostHits = sortedStreetHits[0][1]
-    # print("FIRST 10 SORTED STREET HITS", sortedStreetHits[:10])
     sortedCarStarts = sorted(carStarts.items(), key=lambda v: v[1], reverse=True)
     mostStarts = sortedCarStarts[0][1]
-    # print("FIRST 10 SORTED CAR STARTS", sortedCarStarts[:10])
-
     numNodes = DG.number_of_nodes()
     intersections = []
     incomingStreets = []
@@ -103,21 +83,13 @@ def main(filename):
         for _, _, data in DG.in_edges(n, data=True):
             numIncomingIntersections += 1
             duration = str(assignTimeVal(data['name'], intersectionMaxCarStarts, intersectionMaxStreetHits, intersectionMaxStreetTime))
-            orderList.append((data['name'], duration)) # assigning the time value to the street name for our output
-
+            orderList.append((data['name'], duration))
         incomingStreets.append(numIncomingIntersections)
         orders.append(orderList)
-    # print("intersections are: ", intersections)
-    # print("incomingStreets are: ", incomingStreets)
-    # print("orders are: ", orders)
-
     # create submission file
     create_submit_file(filename, numNodes, intersections, incomingStreets, orders)
 
 def assignTimeVal(streetName, maxCarStarts, maxStreetHits, maxStreetTime):
-    #print('max car starts: ', maxCarStarts)
-    #print('max street hits ', maxStreetHits)
-    #print('max stret time', maxStreetTime)
     if streetName in streetHits:
         hits = streetHits[streetName]
     else:
@@ -126,23 +98,18 @@ def assignTimeVal(streetName, maxCarStarts, maxStreetHits, maxStreetTime):
         starts = carStarts[streetName]
     else: 
         starts = 0
-    
     if maxCarStarts == 0:
         maxCarStarts = 1000000000
-
     if maxStreetHits == 0:
         maxStreetHits = 1000000000
-
     time = streetTimes[streetName]
     startsRatio = float(starts)/float(maxCarStarts)
     hitsRatio = float(hits)/float(maxStreetHits)
     timeRatio = float(time)/float(maxStreetTime)
-
     duration = startsRatio * 0.2 +  hitsRatio * 2 + timeRatio
     if (duration < 1):
         duration = 1
     return int(duration)
-
 
 if __name__ == "__main__":
     filenames = ["exampleInput.txt", "byTheOcean.txt", "checkmate.txt", "dailyCommute.txt", "etoile.txt", "foreverJammed.txt"]
