3a8c8f1028e60c1f773402fe358105347df5f0a5
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2021-02-25 13:28:29 -0500

    input parser

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
+import networkx as nx
+
 def main():
-    print("Hello World!")
-    f = open("lorem-ipsum.txt", "r")
-    print(f.read())
+    G = nx.Graph()
+    f = open("exampleInput.txt", "r")
+    Lines = f.readlines()
+    count = 0
+    duration = 0
+    numIntersections = 0
+    numStreets = 0
+    numCars = 0
+    bonus = 0
+    # Strips the newline character
+    for line in Lines:
+        count += 1
+        if count == 1:
+            duration, numIntersections, numStreets, numCars, bonus = line.split()
+            print("\n")
+            print("-------------------------------------")
+            print("duration is: ", duration)
+            print("numIntersections is: ", numIntersections)
+            print("numStreets is: ", numStreets)
+            print("numCars is: ", numCars)
+            print("bonus is: ", bonus)
+            print("-------------------------------------")
+        elif (count <= 1 + int(numStreets)):
+            startIntersect, endIntersect, streetName, streetTime = line.split()
+            print("streetName is: ", streetName)
+            print("start intersection is: ", startIntersect)
+            print("end intersection is: ", endIntersect)
+            print("streetTime is: ", streetTime)
+            G.add_edge(startIntersect, endIntersect)
+        else:
+            carInfo = line.split()
+            numStreetsCarWantsToTravel = carInfo[0]
+            print("number of streets car wants to travel is: ", numStreetsCarWantsToTravel)
+            for i in range(1,len(carInfo)):
+                print("streetName car wants to travel is: ", carInfo[i])
+        print(f.read())
 
 if __name__ == "__main__":
     main()
